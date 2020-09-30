854a361e43b5a4890f16a2dcc88ebfc4d8088539
Neil <z5202dbc@gmail.com>
2020-09-30 22:57:05 +0800

    試做 Tab 換頁

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import HomeScreen from './src/screens/HomeScreen'
+import ProfileScreen from './src/screens/ProfileScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='Profile'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, focused }) => {
+            let iconName
+            if (route.name == 'Home') {
+              return <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/2/2d/Basic_green_dot.png' }}
+                style={{ width: 30, height: 30 }}
+              />
+              // iconName = focused ? 'ios-trophy' : 'ios-information-circle-outline'
+            } else if (route.name == 'Profile') {
+              iconName = 'ios-options'
+              return <Ionicons name={iconName} size={25} color={color} />
+            }
+
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
