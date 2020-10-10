162302cf8b1ec5c0c604f8b4e9975e9464f1a0e1
Neil <z5202dbc@gmail.com>
2020-10-10 17:46:09 +0800

    Start 2-6

--- App.js
+++ App.js
@@ -1,53 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+import HomeScreen from './src/screens/HomeScreen'
+import NoticeDetailScreen from './src/screens/NoticeDetailScreen'
+import ProfileDetailScreen from './src/screens/ProfileDetailScreen'
+import ProfileScreen from './src/screens/ProfileScreen'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
 
-  // const [name, setName] = useState('')
+  function HomeStack() {
+    return (
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerTitle: '首頁',
+          headerStyle: { backgroundColor: 'tomato' },
+          headerBackTitle: '返回',
+          headerTintColor: 'white'
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="NoticeDetail" component={NoticeDetailScreen} />
+      </Stack.Navigator>
+    )
+  }
 
-  // useEffect(() => {
-  //   console.log('useEffect 內', name)
-  // }, [name])
-
-  // const changeName = () => {
-  //   console.log('changeName 前', name)
-  //   setName('Neil')
-  //   console.log('changeName 後', name)
-  // }
-
-  const [count, setCount] = useState(1)
-
-  useEffect(() => {
-    console.log('useEffect 內', count)
-    const myCount = setInterval(() => {
-      setCount(count + 1)
-    }, 1000)
-    return () => {
-      clearInterval(myCount)
-      console.log('clearInterval')
-    }
-  }, [count])
+  function ProfileStack() {
+    return (
+      <Stack.Navigator
+        initialRouteName='Profile'
+        screenOptions={{
+          headerTitle: '個人檔案',
+          headerStyle: { backgroundColor: 'teal' },
+          headerBackTitle: '返回',
+          headerTintColor: 'white'
+        }}
+      >
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="ProfileDetail" component={ProfileDetailScreen} />
+      </Stack.Navigator>
+    )
+  }
 
   return (
-    <View style={styles.container}>
-      {console.log('render 內', count)}
-      <Text>{count}</Text>
-      {/* <Button
-        title={'賦名'}
-        onPress={() => changeName()}
-      /> */}
-    </View>
+    <NavigationContainer>
+
+      <Tab.Navigator
+        initialRouteName='Home'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            let iconName
+            if (route.name == '首頁') {
+              iconName = 'ios-home'
+            }
+            else if (route.name == '個人檔案') {
+              iconName = 'ios-person'
+            }
+            return <Ionicons name={iconName} size={25} color={color} />
+          }
+
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+          labelPosition: 'beside-icon'
+        }}
+      >
+        <Tab.Screen name="首頁" component={HomeStack} />
+        <Tab.Screen name="個人檔案" component={ProfileStack} />
+
+      </Tab.Navigator>
+
+    </NavigationContainer >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
-
